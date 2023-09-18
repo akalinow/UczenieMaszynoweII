c2c3a8e28901b977e60f7798208d9c2c46c04e5f
Artur Kalinowski <Artur.Kalinowski@cern.ch>
2023-09-18 15:41:22 +0200

    * import script for stripping the solutions from a notebook

---
+++ scripts/README.md
@@ -0,0 +1,40 @@
+
+## Students version generation
+
+Students version - stripped from solutions can be generated automatically using the 
+[scripts/makeStudentsVersion.py][scripts/makeStudentsVersion.py] script.
+This script removes code fragments between `#BEGIN_SOLUTION` and `#END_SOLUTION` tags
+and copies it to the [studentsVersions](studentsVersions) directory.
+
+
+Space between tags will be substituted with a single line with three dots: 
+
+``` python
+def classifier (X, threshold):
+    #BEGIN_SOLUTION
+    return X > threshold
+    #END_SOLUTION
+```
+
+The above in student's version will become:
+``` python
+def classifier (X, threshold):
+    ...
+```
+
+After modification one has to push the changes to GitHub manually:
+
+```bash
+git push origin branch_name
+```
+**Note**:
+* ```#END_SOLUTION``` tag can not be last line in a cell. If this is the case put ```pass``` statement after it:
+
+``` python
+def classifier (X, threshold):
+    #BEGIN_SOLUTION
+    return X > threshold
+    #END_SOLUTION
+pass    
+```
+* in the case of spelling mistake in tags the notebook will be broken - check how it looks like in GitHub
---
+++ scripts/makeStudentsVersion.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import os, sys, getopt
+import subprocess
+import glob
+from termcolor import colored
+
+#################################################################
+#################################################################
+def adaptFiles(input_file_names):
+
+    for input_file_name in input_file_names:
+        
+        if not os.path.exists(input_file_name):
+            print(colored("File:","red"),input_file_name, colored("does not exist","red"))
+            continue
+        output_file_name = "studentsVersions/"+input_file_name
+        output_file = open(output_file_name, "w")
+        result = subprocess.run(["awk", " /#BEGIN_SOLUTION/{p=1}/#END_SOLUTION/{p=0;print \"    \\\"...\\\\n\\\", \";next}!p", input_file_name],
+                                text=True, stdout=output_file)
+                            
+#################################################################
+#################################################################
+if __name__ == "__main__":
+
+    opts, args = getopt.getopt(sys.argv[1:],"i:",["input="])
+    
+    for opt, arg in opts:
+      if opt in ("-i", "--input"):
+         input_file_list = arg.split(",")
+    
+    print('Input file list: ', input_file_list)
+    adaptFiles(input_file_list)
+    
+     
+#################################################################
+#################################################################
