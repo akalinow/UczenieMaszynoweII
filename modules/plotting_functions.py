b68345158bd6dcfe300c91d0c198a38eb264c63e
Artur Kalinowski <Artur.Kalinowski@cern.ch>
2023-09-21 13:19:59 +0200

    * split 01 notebook into python and Docker parts * finish first version of the 01 notebook

---
+++ modules/plotting_functions.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_PDF_CDF(z):
+    fig, axes = plt.subplots(1,2, figsize=(12, 6))
+    
+    ##########
+    axes[0].hist(z, bins=50, density=True, label="PDF")
+    x = np.linspace(-3,3, 100)
+    y = 1.0/np.sqrt(2*np.pi)*np.exp(-x**2/2)
+    axes[0].plot(x,y, 'r', label="N(0,1)")
+    axes[0].set_xlabel("z")
+    axes[0].set_ylabel("p(z)")
+    axes[0].legend(loc="upper right");
+    
+    ##########
+    axes[1].hist(z, color='sandybrown', bins=50, cumulative=True, weights = np.full_like(z, 1/z.size), label="CDF")
+    axes[1].plot((0,0), (0,1), color='black')
+    axes[1].plot((-3, 3), (0.5, 0.5), color='black')
+    axes[1].set_xlabel("z")
+    axes[1].set_ylabel(r"$\int_{-\infty}^{z} p(z) dz$")
+    axes[1].legend(loc="upper left");
+    ##########
+    
+    plt.subplots_adjust(bottom=0.15, left=0.05, right=0.95, wspace=0.3)
+    plt.savefig("fig_png/example1.png", bbox_inches="tight")
---
+++ modules/quadratic_equation.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+###############################################
+def quadraticEqGenertor(nSamples):
+    return np.random.default_rng().uniform(-1,1,(nSamples,3))
+###############################################
+def quadraticEqSolution(coeff):
+
+    a = coeff[:,0:1]
+    b = coeff[:,1:2]
+    c = coeff[:,2:3]
+    
+    delta = b**2 - 4*a*c
+    delta = delta.reshape(-1,1)
+    
+    result = np.where(delta>0, np.sqrt(delta), 0.0)
+    result = result*np.array([-1,1])
+    result = (result - b)/(2.0*a)
+    result = np.where(delta>0, result, None)
+    
+    return result 
+###############################################
+def plotQuadraticEqSolvability(data, interactive=False):
+
+    hasSolution = quadraticEqSolution(data) 
+    hasSolution = (hasSolution[:,0]!=None).reshape(-1,1)
+
+    if interactive:
+        import plotly.express as px
+        nPoints = 500
+        fig = px.scatter_3d(x=data[0:nPoints,0], y=data[0:nPoints,1], z=data[0:nPoints,2], 
+                        color=hasSolution[0:nPoints,0], size = np.full((nPoints), 0.1),
+                        labels={"x":"a", "y":"b", "z":"c"})
+        fig.show()
+    else:
+        fig = plt.figure(figsize=(8,8))
+        axis = fig.add_subplot(projection='3d')
+        pathes = axis.scatter(data[:,0:1],data[:,1:2], data[:,2:3], c=hasSolution);
+        cbar = fig.colorbar(pathes, aspect = 4, fraction=0.10, pad=0.1, 
+                            boundaries = (-0.5,0.5,1.5),
+                            ticks = (0.0, 1.0),
+                            label="has solution? 0/1")
+        axis.set_xlabel('a')
+        axis.set_ylabel('b')
+        axis.set_zlabel('c')
+###############################################
+###############################################
